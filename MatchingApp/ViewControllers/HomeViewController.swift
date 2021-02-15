d661349720691984fdc3f2d1a0f45b1510f5141c
syogo-user <s.photo.trumpet.005@gmail.com>
2021-02-15 21:06:23 +0900

    レイアウトを３分割して作成 画面下部のボタン作成

---
+++ MatchingApp/ViewControllers/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  MatchingApp
+//
+//  Created by 小野寺祥吾 on 2021/02/14.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        let view1 = UIView()
+        view1.backgroundColor = .yellow
+        let view2 = UIView()
+        view2.backgroundColor = .blue
+        let view3 = BottomControlView()
+        let stackView  = UIStackView(arrangedSubviews: [view1,view2,view3])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical //縦
+        self.view.addSubview(stackView)
+        
+
+        [
+            view1.heightAnchor.constraint(equalToConstant:100),
+            view3.heightAnchor.constraint(equalToConstant: 120),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor)].forEach {$0.isActive = true}
+        
+    }
+
+
+}
+
