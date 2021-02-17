9f81fe5d49db662da8e3135a814e4d98c41166b1
syogo-user <s.photo.trumpet.005@gmail.com>
2021-02-17 14:30:19 +0900

    カードをスワイプしたときに円を描くように移動するように修正 GOOD,NOPEのテキストの設定

--- MatchingApp/Views/CardView.swift
+++ MatchingApp/Views/CardView.swift
@@ -11,7 +11,9 @@ class CardView:UIView{
         let iv = UIImageView()
         iv.backgroundColor = .blue
         iv.layer.cornerRadius = 10
-        iv.contentMode = .scaleToFill
+        iv.contentMode = .scaleAspectFill
+        iv.image = UIImage(named: "night sky")
+        iv.clipsToBounds = true
         return iv
     }()
     
@@ -56,6 +58,30 @@ class CardView:UIView{
         return label
     }()
     
+    let goodLabel :UILabel = {
+       let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 45)
+        label.text = "GOOD"
+        label.textColor = UIColor.rgb(red:137,green: 223,blue: 86)
+        label.layer.borderWidth = 3
+        label.layer.borderColor = UIColor.rgb(red:137,green: 223,blue: 86).cgColor
+        label.layer.cornerRadius = 10
+        label.textAlignment = .center
+        label.alpha = 0
+        return label
+    }()
+    let nopeLabel :UILabel = {
+       let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 45)
+        label.text = "NOPE"
+        label.textColor = UIColor.rgb(red:222,green: 110,blue: 110)
+        label.layer.borderWidth = 3
+        label.layer.borderColor = UIColor.rgb(red:222,green: 110,blue: 110).cgColor
+        label.layer.cornerRadius = 10
+        label.textAlignment = .center
+        label.alpha = 0
+        return label
+    }()
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupLayout()
@@ -68,12 +94,36 @@ class CardView:UIView{
         
         if gesture.state == .changed{
             //動かしているとき
-            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+            self.handlePanChange(translation: translation)
         }else if gesture.state == .ended{
-            UIView.animate(withDuration: 0.3) {
-                self.transform = .identity //もとの位置に戻す
-                self.layoutIfNeeded()
-            }
+            self.handlePnaEnded()
+        }
+    }
+    private func handlePanChange(translation:CGPoint){
+        //カードの動き
+        let degree : CGFloat = translation.x / 20
+        let angle = degree * .pi / 100  //回転
+        let rotateTranslation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotateTranslation.translatedBy(x: translation.x, y: translation.y)
+        
+        let ratio :CGFloat = 1 / 100
+        let ratioValue = ratio * translation.x
+        if translation.x > 0 {
+            //右側に移動しているとき
+            self.goodLabel.alpha = ratioValue
+        } else if translation.x < 0{
+            //左側に移動しているとき
+            self.nopeLabel.alpha = -ratioValue
+        }
+        
+    }
+    private func handlePnaEnded(){
+        //カードのもとに戻る動き
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.7, options: []) {
+            self.transform = .identity //もとの位置に戻す
+            self.layoutIfNeeded()
+            self.goodLabel.alpha = 0
+            self.nopeLabel.alpha = 0
         }
     }
 
@@ -88,10 +138,16 @@ class CardView:UIView{
         addSubview(cardImageView)
         addSubview(nameLabel)
         addSubview(baseStackView)
+        addSubview(goodLabel)
+        addSubview(nopeLabel)
+        
         cardImageView.anchor(top:topAnchor,bottom:bottomAnchor,left: leftAnchor,right: rightAnchor,leftPdding: 10,rightPdding: 10)
         infoButton.anchor(width:40)
         baseStackView.anchor(bottom:cardImageView.bottomAnchor,left:cardImageView.leftAnchor,right: cardImageView.rightAnchor,bottomPdding: 20,leftPdding: 20,rightPdding: 20)
         nameLabel.anchor(bottom:baseStackView.topAnchor,left:cardImageView.leftAnchor,bottomPdding: 10,leftPdding: 20)
+        
+        goodLabel.anchor(top:cardImageView.topAnchor,left:cardImageView.leftAnchor,width: 140,height: 55,topPdding: 25 ,leftPdding: 20)
+        nopeLabel.anchor(top:cardImageView.topAnchor,right:cardImageView.rightAnchor,width:140,height: 55,topPdding: 25,rightPdding: 20)
     }
     
     required init?(coder: NSCoder) {
